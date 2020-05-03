049252df837edd817ecb26ea26611f384c77cf43
jakubsutta <60886623+jakubsutta@users.noreply.github.com>
2020-05-03 14:06:16 +0200

    Add files via upload

--- sutta_wordcounter.py
+++ sutta_wordcounter.py
@@ -1,78 +1,81 @@
 import argparse
-import sys
 
 #funkce v mainu
 def main():
     
     #vstupní soubor a parametry
     muj_parser = argparse.ArgumentParser(description='Wordcounter')
-    muj_parser.add_argument("nazev_souboru", help='nazev souboru ke cteni')
+    muj_parser.add_argument("nazev", help='nazev souboru ke cteni')
     muj_parser.add_argument("slova", help='Počítání slov', action = "store_true")
     muj_parser.add_argument("znaky", help='Počítání znaků', action = "store_true")
     muj_parser.add_argument("radky", help='Počítání řádků', action = "store_true")
 
     argumenty = muj_parser.parse_args()
     
-    try:
-        soubor = open(argumenty.nazev_souboru)
+    #podminky
+    try:    
+    
+        #otevreni a precteni souboru
+        soubor = open(argumenty.nazev)
         text = soubor.read()
-        
+    
         if argumenty.slova and argumenty.znaky and argumenty.radky:
             znaky = len(text)
             radky = len(text.split("\n"))
             slova = len(text.split(" ")) + (radky - 1)
             print(f"\n{text}\nPočet znaků: {znaky} , počet slov: {slova} , počet řádků: {radky} \n")
-            file.close() #vždy soubor zavřít
+            soubor.close() #vždy soubor zavřít
             
         elif argumenty.znaky:
             znaky = len(text)
             print(f"\n{text}\nPočet znaků: {znaky}\n")
-            file.close()
+            soubor.close()
         
         elif argumenty.radky:
             radky = len(text.split("\n"))
             print(f"\n{text}\nPočet řádků: {radky} \n")
-            file.close()
+            soubor.close()
         
         elif argumenty.slova:
             radky = len(text.split("\n"))
             slova = len(text.split(" ")) + (radky - 1)
             print(f"\n{text}\nPočet slov: {slova} \n")
-            file.close()            
+            soubor.close()            
         
         elif argumenty.slova and argumenty.znaky:
             znaky = len(text)
             radky = len(text.split("\n"))
             slova = len(text.split(" ")) + (radky - 1)
             print(f"\n{text}\nPočet znaků: {znaky} , počet slov: {slova}\n")
-            file.close()
+            soubor.close()
         
         elif argumenty.radky and argumenty.slova:
             radky = len(text.split("\n"))
             slova = len(text.split(" ")) + (radky - 1)
             print(f"\n{text}\nPočet slov: {slova} , počet řádků: {radky} \n")
-            file.close()
+            soubor.close()
         
         elif argumenty.radky and argumenty.znaky:
             znaky = len(text)
             radky = len(text.split("\n"))
             print(f"\n{text}\nPočet znaků: {znaky} , počet řádků: {radky} \n")
-            file.close()
+            soubor.close()
         
-        else:
+        else:        
             znaky = len(text)
             radky = len(text.split("\n"))
             slova = len(text.split(" ")) + (radky - 1)
             print(f"\n{text}\nPočet znaků: {znaky} , počet slov: {slova} , počet řádků: {radky} \n")
-            file.close()
+            soubor.close()
             
     #hlášení chyb        
     except PermissionError:
         print("Nemáte dostatečné oprávnění.")
     
     except:
-        print("Jedná se o chybný soubor/název souboru.")    
-        
+        print("Jedná se o chybný soubor/název souboru.")
+
+#podminka main        
 if __name__ == '__main__':
     main()
             
